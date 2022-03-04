6464b47a3829aaff29419691d3a8efb70cc2a39e
Nathan-bk <nathan.k.bigaud@gmail.com>
2022-03-04 10:41:27 +0100

    First tests (includding collab)

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=0,1 python run.py \
-    --savedir "directory to save the model" \
-    --model "model type (rnn, rnnsoft, electra)" \
-    --data_dir "root directory of train/valid/test data files" \
-    --adam_path "path to the adam mapping file" \
-    --embs_path "path to pretrained fasttext embeddings" \
-    --data_filename "data file filename (should be the same for train/valid/test)" \
+    --savedir './run_logs' \
+    --model 'electra' \
+    --data_dir './data' \
+    --adam_path './toy_data/valid_adam.txt' \
+    --embs_path './data' \
+    --data_filename 'medal.csv' \
     --epochs 10 \
     --lr 2e-6 \
     --use_scheduler \
@@ -18,3 +18,4 @@ CUDA_VISIBLE_DEVICES=0,1 python run.py \
     --hidden_size 512 \
     --eval_every 200000 \
     # --pretrained_model "path to pretrained model"
+    # Model options : rnn, rnnsoft, electra 
